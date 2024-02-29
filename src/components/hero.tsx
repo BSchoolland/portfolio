699f444a41b3c6cf7d77b695750c625bc3341a27
Benjamin Schoolland <bschoolland@gmail.com>
2024-02-29 12:03:42 -0800

    feat: create title

---
+++ src/components/hero.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section className="">
+        <div className="title-container">
+          <span className="title text-[150px]">Benjamin</span>
+          <span className="title text-[115px]">Schoolland</span>
+          <p className="subtitle text-[32px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+            </p>
+        </div>
+       
+    </section>
+  );
+};
+
+export default HeroSection;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: '#ffffff',
+        secondary: '#001534',
+        decoration: '#00C2FF',
+        highlight: '#003483',
       },
     },
   },
