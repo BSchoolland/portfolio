0373aee1d094c9054cb1e129b233504da882562a
Benjamin Schoolland <bschoolland@gmail.com>
2024-02-29 16:01:15 -0800

    decorative stripes

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const SkillsSection = () => {
-    const skills = ['HTML', 'CSS', 'JavaScript', 'React', 'Next.js'];
+  const skills = ["HTML", "CSS", "JavaScript", "React", "Next.js"];
 
-    return (
-        <section>
-            <div className="decorative-slant">
-                
-            </div>
-            <div className="slant">
-                <div className="inverse-slant">
-            <h2 className='title text-[150px]'>Skills</h2>
-                <ul>
-                    {skills.map((skill, index) => (
-                        <li key={index}>{skill}</li>
-                    ))}
-                </ul>
-                </div>
-            </div>
-            
-            
-            
-        </section>
-    );
+  return (
+    <section>
+      <div className="decorative-slant"></div>
+      <div className="slant">
+      <div className="top-down-stripe-1"></div>
+          <div className="top-down-stripe-2"></div>
+          <div className="top-down-stripe-3"></div>
+          <div className="top-down-stripe-4"></div>
+        <div className="inverse-slant">
+          
+          <h2 className="title text-[150px]">Skills</h2>
+          <ul>
+            {skills.map((skill, index) => (
+              <li key={index}>{skill}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SkillsSection;
